8da82666b120995838d91bcfd327dcfcf86c7d28
dakorpar <dalibor.korpar@gmail.com>
2020-05-06 23:24:35 +0200

    add basic bootstrap cell unit tests

--- tests/Grid/BootstrapCellTest.php
+++ tests/Grid/BootstrapCellTest.php
@@ -5,25 +5,41 @@ namespace Tests\Grid;
 use Contributte\FormsBootstrap\BootstrapForm;
 use Contributte\FormsBootstrap\Grid\BootstrapCell;
 use Contributte\FormsBootstrap\Grid\BootstrapRow;
+use Nette\Application\UI\Presenter;
 use Tests\BaseTest;
 
 class BootstrapCellTest extends BaseTest
 {
 
 	/** @var BootstrapCell */
-	protected $cell;
+	private $cell;
+
+	/** @var BootstrapForm */
+	private $form;
+
+	/** @var BootstrapRow */
+	private $row;
 
 	protected function setUp(): void
 	{
-		$form = new BootstrapForm();
-		$row = new BootstrapRow($form);
-		$this->cell = new BootstrapCell($row, 12);
+		$this->form = new BootstrapForm();
+		$this->row = $this->form->addRow();
+		$this->cell = $this->row->addCell(12);
+		$this->form->setParent($this->createMock(Presenter::class));
 	}
 
 	public function testAddClass(): void
 	{
 		$this->cell->addHtmlClass('new-class');
 		$this->assertEquals('<div class="new-class col-sm-12"></div>', $this->cell->render()->render());
+	}
+
+	public function testAddMultipleButtons(): void
+	{
+		$this->cell->addSubmit('first', 'First');
+		$this->cell->addSubmit('second', 'Second');
+		$this->form->render();
+		$this->expectOutputString($this->loadTextData('cell_with_2_submits.html'));
 	}
 
 }
