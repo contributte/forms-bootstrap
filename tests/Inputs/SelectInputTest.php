05dade305b3a8c7967670ec9a5e9402ac8e8f096
Dalibor Korpar <dalibor.korpar@gmail.com>
2020-06-03 19:26:32 +0200

    add unit test for multidimensional arrays as inputs in select

--- tests/Inputs/SelectInputTest.php
+++ tests/Inputs/SelectInputTest.php
@@ -24,4 +24,31 @@ class SelectInputTest extends BaseTest
 		$this->assertArrayNotHasKey('Croatia', $select->getItems());
 	}
 
+	public function testItemsAsMultiDimensionalArrayWithDuplicateKeysShouldThrowException(): void
+	{
+		$this->expectExceptionMessage('Value "0" is used multiple times.');
+		$countries = [
+			'A' => ['B', 'C'],
+			'D' => ['E', 'F'],
+		];
+
+		$form = new BootstrapForm();
+		$select = $form->addSelect('test', 'test')->setItems($countries, true);
+		 $select->getControl()->render();
+	}
+
+	public function testItemsAsMultiDimensionalArrayRendering(): void
+	{
+		$countries = [
+			'A' => ['B', 'C'],
+			'D' => [2 => 'E', 'F'],
+		];
+
+		$form = new BootstrapForm();
+		$select = $form->addSelect('test', 'test')->setItems($countries, true);
+		$html = $select->getControl()->render();
+		$expectedHtml = '<select name="test" id="frm-test" class="custom-select"><optgroup label="A"><option value="0">B</option><option value="1">C</option></optgroup><optgroup label="D"><option value="2">E</option><option value="3">F</option></optgroup></select>';
+		$this->assertEquals($expectedHtml, $html);
+	}
+
 }
