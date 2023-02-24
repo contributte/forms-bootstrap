5b00b8df36f3fa349626ca756d9e360716717b3c
Dalibor Korpar <dalibor.korpar@gmail.com>
2023-02-24 22:28:06 +0100

    update to nette forms 3.1.10

--- tests/Inputs/RadioInputTest.php
+++ tests/Inputs/RadioInputTest.php
@@ -12,7 +12,7 @@ class RadioInputTest extends BaseTest
 	public function testRadioInput(): void
 	{
 		$form = new BootstrapForm();
-		$input = $form->addRadioList('txt', 'lbl', [1 => 1, 2 => 2]);
+		$input = $form->addRadioList('txt', 'lbl', [1 => '1', 2 => '2']);
 		$this->assertEquals('<fieldset><div class="custom-control custom-radio"><input class="custom-control-input" type="radio" value="1" name="txt" id="frm-txt0" data-nette-rules="[]"><label class="custom-control-label" for="frm-txt0">1</label></div><div class="custom-control custom-radio"><input class="custom-control-input" type="radio" value="2" name="txt" id="frm-txt1"><label class="custom-control-label" for="frm-txt1">2</label></div></fieldset>', (string) $input->getControl());
 	}
 
@@ -21,7 +21,7 @@ class RadioInputTest extends BaseTest
 		BootstrapForm::switchBootstrapVersion(BootstrapVersion::V5);
 
 		$form = new BootstrapForm();
-		$input = $form->addRadioList('txt', 'lbl', [1 => 1, 2 => 2]);
+		$input = $form->addRadioList('txt', 'lbl', [1 => '1', 2 => '2']);
 		$this->assertEquals('<fieldset><div class="form-check"><input class="form-check-input" type="radio" value="1" name="txt" id="frm-txt0" data-nette-rules="[]"><label class="form-check-label" for="frm-txt0">1</label></div><div class="form-check"><input class="form-check-input" type="radio" value="2" name="txt" id="frm-txt1"><label class="form-check-label" for="frm-txt1">2</label></div></fieldset>', (string) $input->getControl());
 
 		BootstrapForm::switchBootstrapVersion(BootstrapVersion::V4);
